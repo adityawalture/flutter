7c5a9ac02e9c0987b8a6850aa9d36591ca1c7074
adi_walture <adityawalture9298@gmail.com>
2022-06-05 00:00:32 +0530

    Hero_animation

---
+++ lib/pages/info_page.dart
@@ -0,0 +1,60 @@
+import 'package:catalog_2/models/catlog.dart';
+import 'package:flutter/material.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+class info_page extends StatelessWidget {
+  final Item catalog;
+
+  const info_page({super.key, required this.catalog}) : assert(catalog != null);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      backgroundColor: Color.fromARGB(255, 237, 230, 230),
+      bottomNavigationBar: ButtonBar(
+        alignment: MainAxisAlignment.spaceBetween,
+        buttonPadding: EdgeInsets.zero,
+        children: [
+          "\$${catalog.price}".text.bold.red600.xl2.make(),
+          ElevatedButton(
+            onPressed: () {},
+            style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.black26),
+                shape: MaterialStateProperty.all(
+                  StadiumBorder(),
+                )),
+            child: "Buy".text.xl.make(),
+          ).wh(100, 45)
+        ],
+      ).p32(),
+      body: SafeArea(
+        bottom: false,
+        child: Column(
+          children: [
+            Hero(
+              tag: Key(catalog.id.toString()),
+              child: Image.network(catalog.imgURL),
+            ).h32(context),
+            Expanded(
+                child: VxArc(
+              height: 30.0,
+              arcType: VxArcType.CONVEY,
+              edge: VxEdge.TOP,
+              child: Container(
+                color: Colors.white,
+                width: context.screenWidth,
+                child: Column(
+                  children: [
+                    catalog.name.text.bold.xl4.make(),
+                    catalog.desc.text.textStyle(context.captionStyle).xl.make(),
+                  ],
+                ).py64(),
+              ),
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
